2ede434a93aa099baa588367e25b7e2172c804f8
박준영 <wnsdud6969@naver.com>
2023-04-20 20:11:42 +0900

    [Feat]: Request start line을 출력하는 printStartLine 함수 추가

--- src/main/java/study/servlet/basic/RequestHeaderServlet.java
+++ src/main/java/study/servlet/basic/RequestHeaderServlet.java
@@ -1,9 +1,31 @@
 package study.servlet.basic;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 @WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
 public class RequestHeaderServlet extends HttpServlet {
 
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        printStartLine(request);
+    }
+
+    private void printStartLine(HttpServletRequest request) {
+        System.out.println("--- REQUEST-LINE - start ---");
+        System.out.println("request.getMethod() = " + request.getMethod());
+        System.out.println("request.getProtocol() = " + request.getProtocol());
+        System.out.println("request.getScheme() = " + request.getScheme());
+        System.out.println("request.getRequestURL() = " + request.getRequestURL());
+        System.out.println("request.getRequestURI() = " + request.getRequestURI());
+        System.out.println("request.getQueryString() = " + request.getQueryString());
+        System.out.println("request.isSecure() = " + request.isSecure());
+        System.out.println("--- REQUEST-LINE - end ---");
+        System.out.println();
+    }
 }
