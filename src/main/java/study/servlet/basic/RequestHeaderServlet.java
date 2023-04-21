14b0101c28c1e97d5fe2b138121e1d6a62fe27cd
박준영 <wnsdud6969@naver.com>
2023-04-21 22:54:18 +0900

    [Feat]: Request Header를 출력하는 printHeaders 함수 추가

--- src/main/java/study/servlet/basic/RequestHeaderServlet.java
+++ src/main/java/study/servlet/basic/RequestHeaderServlet.java
@@ -14,6 +14,7 @@ public class RequestHeaderServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         printStartLine(request);
+        printHeaders(request);
     }
 
     private void printStartLine(HttpServletRequest request) {
@@ -26,6 +27,21 @@ public class RequestHeaderServlet extends HttpServlet {
         System.out.println("request.getQueryString() = " + request.getQueryString());
         System.out.println("request.isSecure() = " + request.isSecure());
         System.out.println("--- REQUEST-LINE - end ---");
+        System.out.println();
+    }
+
+
+    private void printHeaders(HttpServletRequest request) {
+        System.out.println("--- Headers - start ---");
+  /*
+      Enumeration<String> headerNames = request.getHeaderNames();
+      while (headerNames.hasMoreElements()) {
+          String headerName = headerNames.nextElement();
+          System.out.println(headerName + ": " + request.getHeader(headerName));
+      }
+*/
+        request.getHeaderNames().asIterator().forEachRemaining(headerName -> System.out.println(headerName + ":" + request.getHeader(headerName)));
+        System.out.println("--- Headers - end ---");
         System.out.println();
     }
 }
