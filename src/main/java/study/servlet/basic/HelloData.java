93a1786935a6e7c1b102ff7bb35996d09c16c1f8
박준영 <wnsdud6969@naver.com>
2023-04-26 04:27:29 +0900

    [Feat]: JSON 형식 파싱을 위한 HelloData 클래스 추가

---
+++ src/main/java/study/servlet/basic/HelloData.java
@@ -0,0 +1,13 @@
+package study.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloData {
+
+    private String userName;
+    private int age;
+
+}
