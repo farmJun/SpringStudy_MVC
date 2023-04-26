a80ee23a46b2aae6b9dc334fbabab964bbedb663
박준영 <wnsdud6969@naver.com>
2023-04-27 02:50:00 +0900

    [Feat]: Content 편의 메소드 담당 content 함수 추가

--- src/main/java/study/servlet/basic/response/ResponseHeaderServlet.java
+++ src/main/java/study/servlet/basic/response/ResponseHeaderServlet.java
@@ -19,12 +19,24 @@ public class ResponseHeaderServlet extends HttpServlet {
 
         //[response-headers]
         response.setHeader("Content-Type", "text/plain;charset=utf-8");
-        response.setHeader("Cache-Control", "no-cache, no-store, must- revalidate"); response.setHeader("Pragma", "no-cache");
+        response.setHeader("Cache-Control", "no-cache, no-store, must- revalidate");
+        response.setHeader("Pragma", "no-cache");
         response.setHeader("my-header", "hello");
 
         //[Header 편의 메서드] content(response); cookie(response); redirect(response);
+        content(response);
+
         //[message body]
         PrintWriter writer = response.getWriter();
         writer.println("success");
+    }
+
+    private void content(HttpServletResponse response) {
+        //Content-Type: text/plain;charset=utf-8
+        //Content-Length: 2
+        //response.setHeader("Content-Type", "text/plain;charset=utf-8");
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.setContentLength(2); //(생략시 자동 생성)
     }
 }
