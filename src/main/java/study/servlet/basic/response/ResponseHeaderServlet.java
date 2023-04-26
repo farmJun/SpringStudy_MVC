53ac68a003de9c92a7c9033e0928d67ba10ae8c9
박준영 <wnsdud6969@naver.com>
2023-04-27 02:46:07 +0900

    [Feat]: HttpServletResponse 사용법을 확인하는 ResponseHeaderServlet 클래스 추가

---
+++ src/main/java/study/servlet/basic/response/ResponseHeaderServlet.java
@@ -0,0 +1,30 @@
+package study.servlet.basic.response;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "responseHeaderServlet", urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // [status-line]
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        //[response-headers]
+        response.setHeader("Content-Type", "text/plain;charset=utf-8");
+        response.setHeader("Cache-Control", "no-cache, no-store, must- revalidate"); response.setHeader("Pragma", "no-cache");
+        response.setHeader("my-header", "hello");
+
+        //[Header 편의 메서드] content(response); cookie(response); redirect(response);
+        //[message body]
+        PrintWriter writer = response.getWriter();
+        writer.println("success");
+    }
+}
