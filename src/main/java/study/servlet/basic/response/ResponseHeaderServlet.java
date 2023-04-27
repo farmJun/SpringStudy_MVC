d0b9ffde0f22f250082c6d52c4d9cb3acfba61bd
박준영 <wnsdud6969@naver.com>
2023-04-28 01:13:28 +0900

    [Feat]: Redirect 편의 메소드 담당 redirect 함수 추가

--- src/main/java/study/servlet/basic/response/ResponseHeaderServlet.java
+++ src/main/java/study/servlet/basic/response/ResponseHeaderServlet.java
@@ -47,4 +47,13 @@ public class ResponseHeaderServlet extends HttpServlet {
         cookie.setMaxAge(600); //600초
         response.addCookie(cookie);
     }
+
+
+    private void redirect(HttpServletResponse response) throws IOException {
+        //Status Code 302
+        //Location: /basic/hello-form.html
+        //response.setStatus(HttpServletResponse.SC_FOUND); //302
+        //response.setHeader("Location", "/basic/hello-form.html");
+        response.sendRedirect("/basic/hello-form.html");
+    }
 }
