288c4dabcacb9c18c0075cf3e117f813111af398
박준영 <wnsdud6969@naver.com>
2023-05-01 23:00:59 +0900

    [Test]: MemberRepository 클래스 save() 테스트 추가

---
+++ src/test/java/study/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,28 @@
+package study.servlet.domain.member;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberRepositoryTest {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        Member member = new Member("park", 24);
+
+        Member savedMember = memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(savedMember.getId());
+        assertThat(findMember).isEqualTo(savedMember);
+    }
+
+}
