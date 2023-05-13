29f46c477dc6154e0deaac8f6e9be06289d61b5a
박준영 <wnsdud6969@naver.com>
2023-05-13 23:09:04 +0900

    [Feat]: 회원 조회 컨트롤러 MvcMemberListServlet 클래스 추가

---
+++ src/main/java/study/servlet/web/servletmvc/MvcMemberListServlet.java
@@ -0,0 +1,27 @@
+package study.servlet.web.servletmvc;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import study.servlet.domain.member.Member;
+import study.servlet.domain.member.MemberRepository;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MvcMemberListServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("MvcMemberListServlet.service");
+        List<Member> members = memberRepository.findAll();
+        request.setAttribute("members", members);
+        String viewPath = "/WEB-INF/views/members.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
