acf186e4badbb760a63db1e2a281736443ecc5da
박준영 <wnsdud6969@naver.com>
2023-09-08 13:47:04 +0900

    feat: SpringMemberListControllerV1 구현

---
+++ src/main/java/study/servlet/web/springmvc/v1/SpringMemberListControllerV1.java
@@ -0,0 +1,24 @@
+package study.servlet.web.springmvc.v1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import study.servlet.domain.member.Member;
+import study.servlet.domain.member.MemberRepository;
+
+import java.util.List;
+
+@Controller
+public class SpringMemberListControllerV1 {
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members")
+    public ModelAndView process() {
+
+        List<Member> members = memberRepository.findAll();
+
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", members);
+        return mv;
+    }
+}
